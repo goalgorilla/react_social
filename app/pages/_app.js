cf49d2d00a30947524e0db402430a69024b11733
Adam Summerfield <summerfield.adam@gmail.com>
2019-09-17 13:44:51 +0200

    RFE-26 - Added redux wrapper to _app.js.

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,7 +1,10 @@
-import App from "next/app";
+import App, { Container } from "next/app";
 import React from "react";
 import { ThemeProvider } from "styled-components";
 import { createGlobalStyle } from "styled-components";
+import withRedux from "next-redux-wrapper";
+import { initStore } from "../redux";
+import { Provider } from "react-redux";
 
 // Theme from API placeholder
 import themeData from "../static/theme.json";
@@ -18,16 +21,29 @@ const theme = {
   }
 };
 
-export default class MyApp extends App {
-  render() {
-    const { Component, pageProps } = this.props;
-    return (
-      <ThemeProvider theme={theme}>
-        <React.Fragment>
-          <Component {...pageProps} />
-          <GlobalStyle />
-        </React.Fragment>
-      </ThemeProvider>
-    );
+export default withRedux(initStore, { debug: true })(
+  class MyApp extends App {
+    static async getInitialProps({ Component, ctx }) {
+      return {
+        pageProps: {
+          ...(Component.getInitialProps
+            ? await Component.getInitialProps(ctx)
+            : {})
+        }
+      };
+    }
+
+    render() {
+      const { Component, pageProps, store } = this.props;
+      return (
+        <Container>
+          <ThemeProvider theme={theme}>
+            <Provider store={store}>
+              <Component {...pageProps} />
+            </Provider>
+          </ThemeProvider>
+        </Container>
+      );
+    }
   }
-}
+);
