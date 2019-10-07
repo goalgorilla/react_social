c3439e256b7cf92940e55e62bb4f82a90f54355d
Adam Summerfield <summerfield.adam@gmail.com>
2019-10-07 10:18:21 +0200

    RFE-24 - Added Logo component for the navigation bar.

---
+++ app/components/atoms/Logo/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+import Link from "next/link";
+
+const StyledLink = styled(Link)`
+  display: inline-block;
+  vertical-align: top;
+  height: 50px;
+  max-width: 200px;
+  overflow: hidden;
+`;
+
+const StyledImg = styled.img`
+  cursor: pointer;
+
+  &:active {
+    opacity: 0.9;
+  }
+`;
+
+const Logo = () => {
+  return (
+    <StyledLink href="/">
+      <StyledImg src="/static/logo.svg" alt="[Community Name]" />
+    </StyledLink>
+  );
+};
+
+export default Logo;
