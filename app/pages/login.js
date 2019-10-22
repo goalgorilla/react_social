e774d739dc5359896f062dd49ec7b15a20fa0a2d
Adam Summerfield <summerfield.adam@gmail.com>
2019-10-22 11:47:56 +0200

    RFE-121 - Refactored login.js to use hooks instead of class.

--- app/pages/login.js
+++ app/pages/login.js
@@ -1,7 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import actions from "../redux/actions";
-import initialize from "../utils/initialize";
 import Layout from "../components/Layout";
 import Card from "../components/organisms/Card";
 import CardHeader from "../components/atoms/CardHeader";
@@ -34,126 +33,92 @@ const Form = styled.form`
   }
 `;
 
-class Login extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: "",
-      password: "",
-      error: false
-    };
-    this.onChangePassword = this.onChangePassword.bind(this);
-    this.onChangeUsername = this.onChangeUsername.bind(this);
-  }
+function Login(props) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
 
-  static getInitialProps(ctx) {}
-
-  componentDidMount() {
-    this.setState({
-      error: false
-    });
-  }
-
-  componentDidUpdate() {
-    if (this.props.authentication.error && !this.state.error) {
-      this.setState({
-        error: true
-      });
-      this.props.clearAuthenticationStore();
+  // after render, if there is an error in props then:
+  // set error to true to display SystemMessage and clear error from store
+  useEffect(() => {
+    if (props.authentication.error && !error) {
+      setError(true);
+      props.clearAuthenticationStore();
     }
-  }
+  });
 
-  onChangeUsername(e) {
-    this.setState({
-      username: e.target.value
-    });
-  }
-
-  onChangePassword(e) {
-    this.setState({
-      password: e.target.value
-    });
-  }
-
-  handleSubmit(e) {
-    this.setState({
-      error: false
-    });
+  const handleSubmit = e => {
+    setError(false);
     e.preventDefault();
-    this.props.authenticate(
-      { username: this.state.username, password: this.state.password },
-      "login"
-    );
-  }
+    props.authenticate({ username: username, password: password }, "login");
+  };
 
-  render() {
-    return (
-      <Layout title="Login | Open Social">
-        <Wrapper>
-          <Title>Log in</Title>
-          {this.state.error && (
-            <SystemMessage>
-              Oops, there was an error. This may have happened for the following
-              reasons:
-              <br />
-              <br />- Invalid username/email and password combination.
-              <br />- There has been more than one failed login attempt for this
-              account. It is temporarily blocked.
-              <br />- Too many failed login attempts from your computer (IP
-              address). This IP address is temporarily blocked.
-              <br />
-              <br />
-              To solve the issue, try using different login information, try
-              again later, or <b>request a new password</b>
-            </SystemMessage>
-          )}
-          <Form onSubmit={this.handleSubmit.bind(this)}>
-            <Card>
-              <CardHeader>
-                <div>
-                  Log in with <b>username</b> or <b>email</b>
-                </div>
-              </CardHeader>
-              <CardBody>
-                <BlockFormField>
-                  <Label required={true}>Username or email address</Label>
-                  <Input
-                    type="text"
-                    name="username"
-                    value={this.state.username}
-                    onChange={this.onChangeUsername}
-                    required
-                  ></Input>
-                  <InputDescription>
-                    Enter your Open Social username or email.
-                  </InputDescription>
-                </BlockFormField>
-                <BlockFormField>
-                  <Label required={true}>Password</Label>
-                  <Input
-                    type="password"
-                    name="username"
-                    value={this.state.password}
-                    onChange={this.onChangePassword}
-                    required
-                  ></Input>
-                  <InputDescription link={true}>
-                    Forgot your password?
-                  </InputDescription>
-                </BlockFormField>
-              </CardBody>
-              <CardFooter>
-                <div>
-                  Don't have an account yet? <b>Sign Up</b>
-                </div>
-              </CardFooter>
-            </Card>
-            <Button type="submit">Log in</Button>
-          </Form>
-        </Wrapper>
-      </Layout>
-    );
-  }
+  return (
+    <Layout title="Login | Open Social">
+      <Wrapper>
+        <Title>Log in</Title>
+        {error && (
+          <SystemMessage>
+            Oops, there was an error. This may have happened for the following
+            reasons:
+            <br />
+            <br />- Invalid username/email and password combination.
+            <br />- There has been more than one failed login attempt for this
+            account. It is temporarily blocked.
+            <br />- Too many failed login attempts from your computer (IP
+            address). This IP address is temporarily blocked.
+            <br />
+            <br />
+            To solve the issue, try using different login information, try again
+            later, or <b>request a new password</b>
+          </SystemMessage>
+        )}
+        <Form onSubmit={handleSubmit}>
+          <Card>
+            <CardHeader>
+              <div>
+                Log in with <b>username</b> or <b>email</b>
+              </div>
+            </CardHeader>
+            <CardBody>
+              <BlockFormField>
+                <Label required={true}>Username or email address</Label>
+                <Input
+                  type="text"
+                  name="username"
+                  value={username}
+                  onChange={e => setUsername(e.target.value)}
+                  required
+                ></Input>
+                <InputDescription>
+                  Enter your Open Social username or email.
+                </InputDescription>
+              </BlockFormField>
+              <BlockFormField>
+                <Label required={true}>Password</Label>
+                <Input
+                  type="password"
+                  name="password"
+                  value={password}
+                  onChange={e => setPassword(e.target.value)}
+                  required
+                ></Input>
+                <InputDescription link={true}>
+                  Forgot your password?
+                </InputDescription>
+              </BlockFormField>
+            </CardBody>
+            <CardFooter>
+              <div>
+                Don't have an account yet? <b>Sign Up</b>
+              </div>
+            </CardFooter>
+          </Card>
+          <Button type="submit">Log in</Button>
+        </Form>
+      </Wrapper>
+    </Layout>
+  );
 }
 
 export default connect(
