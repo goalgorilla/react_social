83a1045066ee5c07e3d9e638f2e73de106f4a4b7
Adam Summerfield <summerfield.adam@gmail.com>
2019-10-22 15:59:25 +0200

    RFE-127 - Replaced hardcoded API_URL with url obtained from PLATFORM_ROUTES environment variable.

--- app/utils/constants.js
+++ app/utils/constants.js
@@ -1,7 +1,11 @@
 import getConfig from "next/config";
 const { publicRuntimeConfig } = getConfig();
 
-export const API_URL = publicRuntimeConfig.platform_variables.API_URL;
+const API_URL = Object.keys(publicRuntimeConfig.platform_routes).find(url =>
+  url.startsWith("https://api.")
+);
+
+export const API_URL = API_URL;
 export const GRANT_TYPE = publicRuntimeConfig.platform_variables.GRANT_TYPE;
 export const CLIENT_ID = publicRuntimeConfig.platform_variables.CLIENT_ID;
 export const CLIENT_SECRET =
