0d4b072b4b22483d6965e5ac4b444293de13f753
Adam Summerfield <summerfield.adam@gmail.com>
2019-10-23 10:12:02 +0200

    RFE-127 - Removed PLATFORM_ROUTES variable - to be fixed later on.

--- app/utils/constants.js
+++ app/utils/constants.js
@@ -1,22 +1,7 @@
 import getConfig from "next/config";
 const { publicRuntimeConfig } = getConfig();
 
-// Utility to assist in decoding a packed JSON variable.
-function read_base64_json(varName) {
-  try {
-    return JSON.parse(
-      new Buffer.from(process.env[varName], "base64").toString()
-    );
-  } catch (err) {
-    throw new Error(`no ${varName} environment variable`);
-  }
-}
-
-const url = Object.keys(read_base64_json("PLATFORM_ROUTES")).find(url =>
-  url.startsWith("https://api.")
-);
-
-export const API_URL = url;
+export const API_URL = publicRuntimeConfig.platform_variables.API_URL;
 export const GRANT_TYPE = publicRuntimeConfig.platform_variables.GRANT_TYPE;
 export const CLIENT_ID = publicRuntimeConfig.platform_variables.CLIENT_ID;
 export const CLIENT_SECRET =
