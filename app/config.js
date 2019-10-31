f0da83c1b6e588fcdbeb0caa6989a906135e88e4
adam7918 <summerfield.adam@gmail.com>
2019-10-31 11:49:51 +0100

    Removed config.js, not needed.

--- app/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  API: "https://api.feature-rfe-4-xbxsgfy-jmqq2w45dtvdy.eu-4.platformsh.site",
-  grant_type: "password",
-  client_id: "b09f409a-c31b-413b-b210-e7e55ac7422c",
-  client_secret: "1pV4DRYIWo40"
-};
