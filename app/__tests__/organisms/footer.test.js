795851c30f2ef48bba56b14903042707b183010e
adam7918 <summerfield.adam@gmail.com>
2019-10-31 16:02:30 +0100

    RFE-105 - Added footer css test.

--- app/__tests__/organisms/footer.test.js
+++ app/__tests__/organisms/footer.test.js
@@ -19,6 +19,20 @@ describe("Footer renders", () => {
     const { getByTestId } = render(<Footer />);
     expect(getByTestId("footer-nav")).not.toBeNull();
   });
+
+  it("is 100% width at the bottom(0px) of the page", () => {
+    render(<Footer />);
+
+    const footerClass = Footer(
+      "Copyright © 2019. [Community name]. All rights reserved"
+    ).type.styledComponentId;
+    const MyFooterRoots = document.getElementsByClassName(footerClass);
+    const style = window.getComputedStyle(MyFooterRoots[0]);
+
+    expect(style.position).toBe("absolute");
+    expect(style.bottom).toBe("0px");
+    expect(style.width).toBe("100%");
+  });
 });
 
 describe("With React Testing Library Snapshot", () => {
