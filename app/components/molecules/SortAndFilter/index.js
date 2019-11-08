cebb5ab7f52749eff475c52b0e2d969b388fce7d
adam7918 <summerfield.adam@gmail.com>
2019-11-08 11:41:52 +0100

    RFE-6 - Created the sorting and filtering component and added them to profile page events tab and profile page topics tab.

---
+++ app/components/molecules/SortAndFilter/index.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+import Button from "../../atoms/Button";
+
+const Container = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+`;
+
+const StyledButton = styled(Button)`
+  background: white;
+  color: ${props => props.theme.color.text.one};
+  font-weight: bold;
+  border: 0px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  width: 100%;
+
+  &:hover {
+    background: white;
+  }
+
+  &:first-of-type {
+    margin-right: 15px;
+  }
+`;
+
+function SortAndFilter() {
+  return (
+    <Container>
+      <StyledButton>Sort</StyledButton>
+      <StyledButton>Filter</StyledButton>
+    </Container>
+  );
+}
+
+export default SortAndFilter;
--- app/components/organisms/ProfileEvents/index.js
+++ app/components/organisms/ProfileEvents/index.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import Title from "../../atoms/Title";
+import SortAndFilter from "../../molecules/SortAndFilter";
 
 const Container = styled.div`
   margin-top: 60px;
@@ -10,12 +11,32 @@ const BoldTitle = styled(Title)`
   margin-bottom: 20px;
 `;
 
+const HeaderRow = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  & > h2,
+  p {
+    margin-top: 0;
+  }
+`;
+
+const Reset = styled.p`
+  font-weight: ${props => props.theme.font.weight.bold};
+  color: ${props => props.theme.color.brand.primary};
+  cursor: pointer;
+`;
+
 function ProfileEvents(props) {
   const count = 0;
   if (props.activePanel == "events") {
     return (
       <Container>
-        <BoldTitle>{count} Events</BoldTitle>
+        <HeaderRow>
+          <BoldTitle>{count} Events</BoldTitle>
+          <Reset>Reset</Reset>
+        </HeaderRow>
+        <SortAndFilter />
       </Container>
     );
   } else {
