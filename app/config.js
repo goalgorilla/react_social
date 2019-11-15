1e06ba8fcd730c44e73adeecba6905834ad6353a
adam7918 <summerfield.adam@gmail.com>
2019-11-15 12:48:45 +0100

    removed unnecessary / from api url

--- app/config.js
+++ app/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  API: "https://api.master-7rqtwti-jmqq2w45dtvdy.eu-4.platformsh.site/",
+  API: "https://api.master-7rqtwti-jmqq2w45dtvdy.eu-4.platformsh.site",
   grant_type: "password",
   client_id: "b10b793f-e06c-4d10-abb7-b8f2ed3b991f",
   client_secret: "tZSfN3j1l3az"
