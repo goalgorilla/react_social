6efb8e0218740dbdf332af1116ced0bb7a25cbf1
adam7918 <summerfield.adam@gmail.com>
2019-11-19 13:48:11 +0100

    Removed bottom: 0 from Footer

--- app/components/organisms/Footer/index.js
+++ app/components/organisms/Footer/index.js
@@ -8,7 +8,6 @@ import SocialNav from "../../molecules/SocialNav";
 // The footer of the website, contains SocialNav, FooterNav and any children in the form of a <p> tag.
 const Wrapper = styled.div`
   position: relative;
-  bottom: 0;
   width: 100%;
   height: 10rem;
   background: white;
