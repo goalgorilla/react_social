ef29ec2b72fa5d803cc9436a480e00f066729ed8
adam7918 <summerfield.adam@gmail.com>
2019-11-19 14:07:26 +0100

    Change InfoTitle from p to h5.

--- app/components/atoms/InfoTitle/index.js
+++ app/components/atoms/InfoTitle/index.js
@@ -1,9 +1,10 @@
 import styled from "styled-components";
 
-const InfoTitle = styled.p`
+const InfoTitle = styled.h5`
   color: ${props => props.theme.color.text.three};
   font-size: ${props => props.theme.font.size.medium};
   font-weight: ${props => props.theme.font.weight.bold};
+  margin: 5px 0;
 `;
 
 export default InfoTitle;
