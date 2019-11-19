8a7e070e98d209f3a82c03a20d349fcb884f553e
adam7918 <summerfield.adam@gmail.com>
2019-11-19 14:09:07 +0100

    Added comment to InfoTitle to detail the role of the component.

--- app/components/atoms/InfoTitle/index.js
+++ app/components/atoms/InfoTitle/index.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 
+// InfoTitle is used on the information panel on the profile page. It is used to title a row of information (e.g Introduction, email..)
 const InfoTitle = styled.h5`
   color: ${props => props.theme.color.text.three};
   font-size: ${props => props.theme.font.size.medium};
