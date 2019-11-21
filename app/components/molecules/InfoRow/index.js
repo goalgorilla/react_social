c808ceb39b0c5251e939d299ddbb8f27b184f8d6
adam7918 <summerfield.adam@gmail.com>
2019-11-21 13:33:42 +0100

    Add InfoRow comment.

--- app/components/molecules/InfoRow/index.js
+++ app/components/molecules/InfoRow/index.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 
+// InfoRow is used to contain InfoTitle (e.g Email) and the relevant text (e.g. chrishall@gmail.com).
 const InfoRow = styled.div`
   display: flex;
   flex-direction: column;
