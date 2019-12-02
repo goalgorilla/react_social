70bf8c4a809a71d1f9f63e505cb2d7c1b25519a0
adam7918 <summerfield.adam@gmail.com>
2019-12-02 10:01:59 +0100

    Add comment

--- app/components/atoms/Tag/index.js
+++ app/components/atoms/Tag/index.js
@@ -1,5 +1,9 @@
 import styled from "styled-components";
 
+// A tag can be used to highlight information on a card for the user
+// For example on the profile page, in the RecentActivity card a tag
+// is used to show if a user is already enrolled in an event with the message:
+// 'You also enrolled'
 const Tag = styled.p`
   color: ${props => props.theme.color.background.primary};
   font-size: ${props => props.theme.font.size.small};
