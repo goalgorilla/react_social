fb1b41a010d90a73053665091db4b42a14f5306c
adam7918 <summerfield.adam@gmail.com>
2019-12-12 14:33:31 +0100

    Fix typo in import

--- app/pages/search/all.js
+++ app/pages/search/all.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Layout from '../../components/Layoutout';
+import Layout from '../../components/Layout';
 
 function SearchAll() {
   return <Layout title="Search">Search All</Layout>;
