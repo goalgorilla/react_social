bfac9ce6562e0a1aa800a042cd5684efd60ed060
adam7918 <summerfield.adam@gmail.com>
2019-12-12 15:10:55 +0100

    Add ContentRegion and ComplimentaryRegion components
    These are two comments which can be used to display content on the page.

---
+++ app/components/organisms/ComplimentaryRegion/index.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const ComplimentaryRegion = styled.div`
+  margin-top: 1rem;
+  flex: 0 1 33.33333%;
+  order: 2;
+  max-width: 33.33333%;
+`;
+
+export default ComplimentaryRegion;
---
+++ app/components/organisms/ContentRegion/index.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const ContentRegion = styled.div`
+  flex: 0 0 66.66667%;
+  max-width: 66.66667%;
+  order: 1;
+`;
+
+export default ContentRegion;
